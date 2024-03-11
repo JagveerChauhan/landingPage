3f0afa0834f38eca42ebc110a29eae9fe7f1dd05
jagveerChauhan <jagveer@onetick.in>
2024-03-11 11:23:55 +0530

    added on click event of google ads

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -1,3 +1,4 @@
+'use client'
 import Link from "next/link";
 import Image from "next/image";
 
@@ -8,7 +9,7 @@ export default function Navbar()
         <nav className="sticky z-50 top-0 left-0 w-full flex flex-row justify-between drop-shadow-md bg-white px-1 md:px-10">
         <Link className="my-auto" href={'/'}><Image src={'/onetickLogo.png'} alt="Logo" height={45} width={150}></Image></Link>
         <span className="py-6">
-          <Link className="py-1 md:py-3 px-2 md:px-6 bg-dark-blue text-white rounded-2xl font-semibold font-DM Sans" href="tel:+919650076857">Call Now</Link>
+          <Link onClick={()=>"gtag_report_conversion('tel:9650076857')"} className="py-1 md:py-3 px-2 md:px-6 bg-dark-blue text-white rounded-2xl font-semibold font-DM Sans" href="tel:+919650076857">Call Now</Link>
         </span>
       </nav></>
     )
